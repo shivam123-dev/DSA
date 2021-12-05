36c15ee6a7f69856a1e9869c28f72e8e4deb641a
shivam123-dev <ss2249@srmist.edu.in>
2021-12-05 19:50:06 +0530

    DSA

--- 06 Pointers & Dynamic Memory/pointers.cpp
+++ 06 Pointers & Dynamic Memory/pointers.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main(){
     
-    int a = 10;             // initialising variable
-    int *xptr = &a;         // pointer variable
-    int **xxptr = &xptr;    // pointer to pointer variable
-    cout << a << endl;      // gives value of a
-    cout << &a << endl;     // gives the address of a
-    cout << xptr << endl;   // gives the address of a
-    cout << &xptr << endl;  // gives the address of xptr
-    cout << xxptr << endl;  // gives the address of xptr
-    cout << &xxptr << endl; // gives the address of xxptr
+    int a = 10;                     // initialising variable
+    int *xptr = &a;                 // pointer variable
+    int **xxptr = &xptr;            // pointer to pointer variable
+    cout << a << endl;              // gives value of a
+    cout << &a << endl;             // gives the address of a
+    cout << xptr << endl;           // gives the address of a
+    cout << &xptr << endl;          // gives the address of xptr
+    cout << xxptr << endl;          // gives the address of xptr
+    cout << &xxptr << endl;         // gives the address of xxptr
     return 0;
 }
