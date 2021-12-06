9d5ec193ff558de57733a78232d3bfdd6c214c62
shivam123-dev <ss2249@srmist.edu.in>
2021-12-06 16:24:50 +0530

    DSA

---
+++ 06 Pointers & Dynamic Memory/2DdynamicArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int **create2DArray(int rows, int cols){
+    // allocation of 2D arrays
+    int **arr = new int* [rows];
+    for(int i=0;i<rows;i++){
+        arr[i] = new int[cols];
+    }
+    int value = 0;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            arr[i][j] = value;
+            value++;
+        }
+    }
+    return arr;
+}
+int main(){
+    int rows, cols;
+    cin >> rows >> cols;
+    int **arr = create2DArray(rows, cols);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
