c9299c24f23fa94e3dd4840a3a44f2cd8f412e78
shivam123-dev <ss2249@srmist.edu.in>
2021-12-09 19:22:44 +0530

    DSA

--- 08 Bit Manipulation/commonOperations.cpp
+++ 08 Bit Manipulation/commonOperations.cpp
@@ -1,38 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 // for getting the ith bit
-int getithbit(int n, int i){
+int getithbit(int n, int i)
+{
     int mask = (1 << i);
-    return (n&mask) > 0 ? 1:0;
+    return (n & mask) > 0 ? 1 : 0;
 }
 // for setting the ith bit
-int setithbit(int n, int i){
+int setithbit(int n, int i)
+{
     int mask = (1 << i);
-    return n|mask;
+    return n | mask;
 }
 // for clearing ith bit
-int clearithbit(int n, int i){
+int clearithbit(int n, int i)
+{
     int mask = ~(1 << i);
-    return n&mask;
+    return n & mask;
 }
 // for updating ith bit
-int updateithbit(int n, int i, int v){
+int updateithbit(int n, int i, int v)
+{
     clearithbit(n, i);
     int mask = (v << i);
-    return n|mask;  
+    return n | mask;
 }
 // for clearing last I bits
-int clearlastibits(int n, int i){
+int clearlastibits(int n, int i)
+{
     int mask = (-1 << i);
-    return n&mask;
+    return n & mask;
 }
-int main(){
-    int n, pos;
-    cin >> n >> pos;
+// for clearing a range of bits
+int clearRangeofBits(int n, int i, int j)
+{
+    int a = (-1) << (j + 1);
+    int b = (1 << i) - 1;
+    int mask = a | b;
+    return n & mask;
+}
+int main()
+{
+    int n, i, j;
+    cin >> n >> i >> j;
+    // int n, pos;
+    // cin >> n >> pos;
     // cout << getithbit(n, pos) << endl;
     // cout << setithbit(n, pos) << endl;
     // cout << clearithbit(n, pos) << endl;
     // cout << updateithbit(n, pos, 1) << endl;
-    cout << clearlastibits(n, pos) << endl;
-    return 0;    
+    // cout << clearlastibits(n, pos) << endl;
+    cout << clearRangeofBits(n, i, j) << endl;
+    return 0;
 }
