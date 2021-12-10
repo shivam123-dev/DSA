8eb16d04d64fc97a92f70dd7efe1a0d1c2b05f71
shivam123-dev <ss2249@srmist.edu.in>
2021-12-10 12:30:35 +0530

    DSA

--- 08 Bit Manipulation/makeBinary.cpp
+++ 08 Bit Manipulation/makeBinary.cpp
@@ -1,5 +1,21 @@
-#include<iostream>
+// convert decimal number to binary number using bitwise operator
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(void){
-
+int convert(int n)
+{
+    int ans = 0;
+    int power = 1;
+    while(n>0){
+        ans += (n&1)*power;
+        power *= 10;
+        n = n>>1;
+    }
+    return ans;
+}
+int main(void)
+{
+    int n;
+    cin >> n;
+    cout << convert(n) << endl;
 }
