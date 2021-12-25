4af517c092182f3d2d3af2083d710a7065f6fc97
shivam123-dev <ss2249@srmist.edu.in>
2021-12-25 13:49:46 +0530

    DSA

--- 13 Object Oriented Programming/Constructor.cpp
+++ 13 Object Oriented Programming/Constructor.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 class Product{
     int mrp;
     int id;
     int sp;
+    char name[100];
 public:
     Product(){
         cout << "Inside default constructor" << endl;
     }
-    Product(int id, int mrp, int sp){
+    Product(int id, int mrp, int sp, char* name){
         cout << "Inside parameterised constructor" << endl;
         this->id = id;
         this->mrp = mrp;
         this->sp = sp;
+        strcpy(this->name, name);
     }
     int setMrp(int price){
         if(price>0){
@@ -33,12 +36,15 @@ public:
     int getSP(){
         return sp;
     }
+    char* getName(){
+        return name;
+    }
 };
 int main(){
     // Product camera;
     // camera.setMrp(100);
     // camera.setSP(80);
-    Product camera(101, 100, 80);
-    cout << camera.getMrp() << endl << camera.getSP() << endl;
+    Product camera(101, 100, 80, "Sony");
+    cout << camera.getMrp() << endl << camera.getSP() << endl << camera.getName() << endl;
     return 0;
 }
