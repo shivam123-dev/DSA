b81f0d78a34749348ca3684687454e3126d86e13
shivam123-dev <ss2249@srmist.edu.in>
2021-12-26 11:23:05 +0530

    DSA

--- 14 Linked List/InsertionUsing_Head_and_Tail.cpp
+++ 14 Linked List/InsertionUsing_Head_and_Tail.cpp
@@ -10,6 +10,11 @@ public:
         next = NULL;
     }
 };
+void insertAtHead(node* &head, int val){
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+}
 void insertAtTail(node* &head, node* &tail, int val){
     node* n = new node(val);
     if(head == NULL){
@@ -19,6 +24,15 @@ void insertAtTail(node* &head, node* &tail, int val){
     node* temp = tail;
     temp->next = n;
     tail = n;
+}
+void insertInMiddle(node* &head, int val, int pos){
+    node* temp = head;
+    node* n = new node(val);
+    while(temp->data != pos){
+        temp = temp->next;
+    }
+    n->next = temp->next;
+    temp->next = n;
 }
 void displayLL(node* head){
     node* temp = head;
@@ -35,6 +49,10 @@ int main(){
     insertAtTail(head, tail, 2);
     insertAtTail(head, tail, 3);
     insertAtTail(head, tail, 4);
+    displayLL(head);
+    insertAtHead(head, 5);
+    displayLL(head);
+    insertInMiddle(head, 10, 2);
     displayLL(head);
     return 0;
 }
