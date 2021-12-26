09fe76cc797358658dbf1e1d4563e6058970a698
shivam123-dev <ss2249@srmist.edu.in>
2021-12-26 11:46:47 +0530

    DSA

--- 14 Linked List/InsertionUsing_Head_and_Tail.cpp
+++ 14 Linked List/InsertionUsing_Head_and_Tail.cpp
@@ -1,50 +1,60 @@
 // Using both head and tail pointers
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
+class node
+{
 public:
     int data;
-    node* next;
-    node(int val){
+    node *next;
+    node(int val)
+    {
         data = val;
         next = NULL;
     }
 };
-void insertAtHead(node* &head, int val){
-    node* n = new node(val);
+void insertAtHead(node* &head, int val)
+{
+    node *n = new node(val);
     n->next = head;
     head = n;
 }
-void insertAtTail(node* &head, node* &tail, int val){
-    node* n = new node(val);
-    if(head == NULL){
+void insertAtTail(node* &head, node* &tail, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
         head = tail = n;
         return;
     }
-    node* temp = tail;
+    node *temp = tail;
     temp->next = n;
     tail = n;
 }
-void insertInMiddle(node* &head, int val, int pos){
-    node* temp = head;
-    node* n = new node(val);
-    while(temp->data != pos){
+void insertInMiddle(node* &head, int val, int pos)
+{
+    node *temp = head;
+    node *n = new node(val);
+    while (temp->data != pos)
+    {
         temp = temp->next;
     }
     n->next = temp->next;
     temp->next = n;
 }
-void displayLL(node* head){
-    node* temp = head;
-    while(temp){
+void displayLL(node *head)
+{
+    node *temp = head;
+    while (temp)
+    {
         cout << temp->data << "->";
         temp = temp->next;
     }
     cout << "NULL" << endl;
 }
-int main(){
-    node* head = NULL;
-    node* tail = NULL;
+int main()
+{
+    node *head = NULL;
+    node *tail = NULL;
     insertAtTail(head, tail, 1);
     insertAtTail(head, tail, 2);
     insertAtTail(head, tail, 3);
