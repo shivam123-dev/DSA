e68adfe40370864fb90e07d9863043d56820906c
shivam123-dev <ss2249@srmist.edu.in>
2021-12-26 11:01:02 +0530

    DSA

---
+++ 14 Linked List/InsertatTail.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class node{
+public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+void insertAtTail(node* &head, int val){
+    node* temp = new node(val);
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+    node* n = head;
+    while(n->next!=NULL){
+        n = n->next;
+    }
+    n->next = temp;
+}
+void displayLL(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main(){
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    displayLL(head);
+    return 0;
+}
